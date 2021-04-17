438dfc1295b718ebba0af6627b31086863a72588
Elias Calagiu <eliasc81@gmail.com>
2021-04-17 12:28:54 -0700

    added namespace comments

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
 namespace CatWorx.BadgeMaker
-{
+// namespaces are like containers that have members
+// a "member" can be another nested namespace, a method, or a class
+{ 
+    // everything inside these curly braces can be interpreted as members of this namespace
+    // Common C# conventions are to use a company name for the root namespace in order to ensure that it is unique.
     class Program
     {
         static void Main(string[] args)
